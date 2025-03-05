7ef06bb34beaf2b84a6af1c884df8c0e6e49dd53
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-05 17:45:00 -0300

    chore: specific not authenticated mapped exception

---
+++ Specifics/NotAuthenticatedMappedException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.Utils.MappedExceptions.Specifics
+{
+    public class NotAuthenticatedMappedException : MappedException
+    {
+        public NotAuthenticatedMappedException(string briefPublicMessage) : base(briefPublicMessage)
+        {
+        }
+
+        public NotAuthenticatedMappedException(string briefPublicMessage, string details) : base(briefPublicMessage, details)
+        {
+        }
+
+        public NotAuthenticatedMappedException(string briefPublicMessage, Exception originalException) : base(briefPublicMessage, originalException)
+        {
+        }
+
+        public NotAuthenticatedMappedException(string briefPublicMessage, string details, Exception originalException) : base(briefPublicMessage, details, originalException)
+        {
+        }
+    }
+}
