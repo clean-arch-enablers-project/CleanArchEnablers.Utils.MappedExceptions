328a2e29a18f577471f4092a4feab0195b9a0a96
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-05 17:46:11 -0300

    chore: specific not found mapped exception

---
+++ Specifics/NotFoundMappedException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.Utils.MappedExceptions.Specifics
+{
+    public class NotFoundMappedException : MappedException
+    {
+        public NotFoundMappedException(string briefPublicMessage) : base(briefPublicMessage)
+        {
+        }
+
+        public NotFoundMappedException(string briefPublicMessage, string details) : base(briefPublicMessage, details)
+        {
+        }
+
+        public NotFoundMappedException(string briefPublicMessage, Exception originalException) : base(briefPublicMessage, originalException)
+        {
+        }
+
+        public NotFoundMappedException(string briefPublicMessage, string details, Exception originalException) : base(briefPublicMessage, details, originalException)
+        {
+        }
+    }
+}
