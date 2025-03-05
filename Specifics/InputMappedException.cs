1782aee9b4ab8c319eb3349e9d19279ce2118a3b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-05 17:41:37 -0300

    chore: specific input mapped exception

---
+++ Specifics/InputMappedException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.Utils.MappedExceptions.Specifics
+{
+    public class InputMappedException : MappedException
+    {
+        public InputMappedException(string briefPublicMessage) : base(briefPublicMessage)
+        {
+        }
+
+        public InputMappedException(string briefPublicMessage, string details) : base(briefPublicMessage, details)
+        {
+        }
+
+        public InputMappedException(string briefPublicMessage, Exception originalException) : base(briefPublicMessage, originalException)
+        {
+        }
+
+        public InputMappedException(string briefPublicMessage, string details, Exception originalException) : base(briefPublicMessage, details, originalException)
+        {
+        }
+    }
+}
